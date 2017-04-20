919171e5d3c4298b9aea4d7ac5586eb88f0becf1
David M. Rogers <predictivestatmech@gmail.com>
2017-04-19 23:35:47 -0400

    Implemented gather/scatter atoms.

--- src.c
+++ src.c
@@ -228,25 +228,84 @@ int global(sil_State *S) {
     return 0;
 }
 
-// LAMMPS -> String -> a:* -> a
-// extract atom-based quantities.
-int extract_atom(sil_State *S) {
-    lmp_str;
-
-    int r = sil_tointeger(S, 3);
-    if(!(r == 6 || r == 7)) {
-        return sil_err(S, "lammps.extract_atom: invalid return type");
+// String prop, Kind t, Vector v -> ST(LAMMPS, Nil)
+int scatter_atoms(sil_State *S) {
+    size_t len;
+    LAMMPS *lmp = (LAMMPS *)sil_getST(S, &len);
+    if(lmp == NULL) {
+        return sil_err(S, "Invalid LAMMPS ST");
+    }
+    int t      = sil_tointeger(S, 2);
+    const vector *v  = (const vector *)sil_topointer(S, 3);
+    int atoms = lammps_get_natoms(lmp->lmp);
+    int count  = v->n / atoms;
+    if(!(t == 6 || t == 7)) {
+        return sil_err(S, "scatter_atoms: invalid type");
+    }
+    if(count*atoms != v->n) {
+        return sil_err(S, "scatter_atoms: vector length is not a multiple"
+                          " of %d atoms", atoms);
+    }
+    char *name = sil_toastring(S, 1);
+    if(name == NULL) {
+        return sil_err(S, "scatter_atoms: invalid property name");
     }
 
-    void *ret = lammps_extract_atom(lmp->lmp, name);
+    if(type == 6) {
+        int *y = (int *)malloc(sizeof(int)*v->n);
+        for(int i=0; i<v->n; i++) {
+            y[i] = v->x[i];
+        }
+        lammps_scatter_atoms(lmp->lmp, name, 0, count, y);
+        free(y);
+    } else {
+        lammps_scatter_atoms(lmp->lmp, name, 1, count, v->x);
+    }
+
     free(name);
+    sil_settop(S, 0);
+    sil_pushnil();
+    return 0;
+}
+
+
+
+/* TODO: add scatter_atoms
+ *       implement a mechanism for inferring '+' between vectors
+ *       with different implementation types...
+ *       implement set_global?
+ */
+// LAMMPS lmp, String name, Kind t, Int count -> Vector
+// count is number of values per atom (usu. 1 or 3)
+int gather_atoms(sil_State *S) {
+    lmp_str;
+    int t = sil_tointeger(S, 3);
+    int count = sil_tointeger(S, 4);
+    int n = lammps_get_natoms(lmp->lmp)*count;
+
+    if(count < 1 || count > 100) {
+        free(name);
+        return sil_err(S, "lammps.gather_atoms: invalid count");
+    }
+    if(!(t == 6 || t == 7)) { // 6 ~> 0, 7 ~> 1
+        free(name);
+        return sil_err(S, "lammps.gather_atoms: invalid return type");
+    }
+
+    vector *v = (vector *)malloc(sizeof(vector) + 8*n);
+
+    lammps_gather_atoms(lmp->lmp, name, t-6, count, v->x);
+    free(name);
+    if(t == 6) { // convert to doubles in-place
+        // assuming sizeof(double) >= sizeof(int)
+        int *y = (int *)v->x;
+        for(int i=n-1; i >= 0; i--) {
+            v->x[i] = y[i];
+        }
+    }
 
     sil_settop(S, 0);
-    if(r == 7) {
-        sil_pushdouble(S, *(double *)ret);
-    } else {
-        sil_pushinteger(S, *(int *)ret);
-    }
+    sil_pushvector(S, v);
     return 0;
 }
 
@@ -297,6 +356,7 @@ int gather_atoms(sil_State *S) {
     if(v == NULL) {
         return sil_err(S, "lammps.gather_atoms: Memory error");
     }
+    v->n = n;
     lammps_gather_atoms(lmp->lmp, name, t == 7, count, v->x);
 
     free(name);
